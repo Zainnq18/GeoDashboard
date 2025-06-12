75bd8384cd7fec6eb0cb712800bf3b927370740f
Zain Qureshi <zainn.qureshi18@gmail.com>
2025-06-12 16:44:54 +0530

    12/15
    UPLOADED

---
+++ state_population_crores.js
@@ -0,0 +1,146 @@
+var state_population_crores = [
+  {
+    "stname": "Uttar Pradesh",
+    "Population": 19.98
+  },
+  {
+    "stname": "Maharashtra",
+    "Population": 11.24
+  },
+  {
+    "stname": "Bihar",
+    "Population": 10.41
+  },
+  {
+    "stname": "West Bengal",
+    "Population": 9.13
+  },
+  {
+    "stname": "Madhya Pradesh",
+    "Population": 7.26
+  },
+  {
+    "stname": "Tamil Nadu",
+    "Population": 7.21
+  },
+  {
+    "stname": "Rajasthan",
+    "Population": 6.85
+  },
+  {
+    "stname": "Karnataka",
+    "Population": 6.11
+  },
+  {
+    "stname": "Gujarat",
+    "Population": 6.04
+  },
+  {
+    "stname": "Andhra Pradesh",
+    "Population": 4.96
+  },
+  {
+    "stname": "Odisha",
+    "Population": 4.2
+  },
+  {
+    "stname": "Telangana",
+    "Population": 3.5
+  },
+  {
+    "stname": "Kerala",
+    "Population": 3.34
+  },
+  {
+    "stname": "Jharkhand",
+    "Population": 3.3
+  },
+  {
+    "stname": "Assam",
+    "Population": 3.12
+  },
+  {
+    "stname": "Punjab",
+    "Population": 2.77
+  },
+  {
+    "stname": "Chhattisgarh",
+    "Population": 2.55
+  },
+  {
+    "stname": "Haryana",
+    "Population": 2.54
+  },
+  {
+    "stname": "Uttarakhand",
+    "Population": 1.01
+  },
+  {
+    "stname": "Himachal Pradesh",
+    "Population": 0.69
+  },
+  {
+    "stname": "Tripura",
+    "Population": 0.37
+  },
+  {
+    "stname": "Meghalaya",
+    "Population": 0.3
+  },
+  {
+    "stname": "Manipur",
+    "Population": 0.26
+  },
+  {
+    "stname": "Nagaland",
+    "Population": 0.2
+  },
+  {
+    "stname": "Goa",
+    "Population": 0.15
+  },
+  {
+    "stname": "Arunachal Pradesh",
+    "Population": 0.14
+  },
+  {
+    "stname": "Mizoram",
+    "Population": 0.11
+  },
+  {
+    "stname": "Sikkim",
+    "Population": 0.06
+  },
+  {
+    "stname": "Delhi",
+    "Population": 1.68
+  },
+  {
+    "stname": "Jammu and Kashmir",
+    "Population": 1.23
+  },
+  {
+    "stname": "Puducherry",
+    "Population": 0.12
+  },
+  {
+    "stname": "Chandigarh",
+    "Population": 0.11
+  },
+  {
+    "stname": "Dadra and Nagar Haveli and Daman and Diu",
+    "Population": 0.06
+  },
+  {
+    "stname": "Andaman and Nicobar Islands",
+    "Population": 0.04
+  },
+  {
+    "stname": "Ladakh",
+    "Population": 0.03
+  },
+  {
+    "stname": "Lakshadweep",
+    "Population": 0.01
+  }
+];
